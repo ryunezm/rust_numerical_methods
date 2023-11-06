e17939f8164ea91b6321618576faed6da02bdd2a
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-11-06 11:56:58 -0500

    --> To structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod bisection;
 fn main() {
-    println!("Hello, world!");
+
+    println!("NUMERICAL METHODS");
+    println!("1. Bisection");
+    println!("2. Euler");
+    println!("3. Polynomial interpolation");
+    println!("4. Simpson");
+    println!("5. Jacobi");
+    println!("6. Gauss-Seidel");
+    println!("7. Gauss-Jordan");
+    println!("8. Newton Raphson");
+
 }
+
+//TODO
+fn nm_euler(){}
+
+//TODO
+fn nm_polynomial_interpolation(){}
+
+//TODO
+fn nm_simpson(){}
+
+//TODO
+fn nm_jacobi(){}
+
+//TODO
+fn nm_gauss_seidel(){}
+
+//TODO
+fn nm_gauss_jordan(){}
+
+//TODO
+fn nm_newton_raphson(){}
+
+//TODO
+fn nm_rk4(){}
