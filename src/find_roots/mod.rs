f83247999c193dea464be5ef2af3b0d43d0170e2
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-11-09 17:13:14 -0500

    --> Cleaning and preparing code for numerical methods.

--- src/find_roots/mod.rs
+++ src/find_roots/mod.rs
@@ -1,8 +1,3 @@
 pub mod find_root_bracketing;
 pub mod find_root_iterative;
 pub mod find_root_combination;
-
-//Re-exporting the FindRoot trait here if necessary
-pub use find_root_bracketing::Bisection;
-pub use find_root_bracketing::FalsePosition;
-pub use find_root_bracketing::ITP;
