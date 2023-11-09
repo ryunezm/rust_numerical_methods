ce38a8263c9bd1e88d8488c1945f4788ed8a881d
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-11-09 15:10:16 -0500

    --> Cleaning and preparing code.

--- src/find_root_iterative.rs
+++ src/find_root_iterative.rs
@@ -13,6 +13,12 @@ pub struct Secant
 pub struct Steffensen
 { function: fn(f64) -> f64, x0: f64, tolerance: f64, max_iter: usize }
 
+pub struct FixedPoint
+{ function: fn(f64) -> f64, x0: f64, tolerance: f64, max_iter: usize }
+
+pub struct InverseQuadraticInterpolation
+{ function: fn(f64) -> f64, x0: f64, x1: f64, x2: f64, tolerance: f64, max_iter: usize }
+
 // ------ IMPLEMENTATIONS ------
 
 impl Newton {
@@ -90,5 +96,63 @@ impl FindRoot for Steffensen {
             iter+=1;
         }
         x
+    }
+}
+
+impl FixedPoint {
+    fn new(function: fn(f64) -> f64, x0: f64, tolerance: f64, max_iter: usize)
+        -> Self { FixedPoint{ function, x0, tolerance, max_iter } }
+}
+
+impl FindRoot for FixedPoint{
+    fn solve(&mut self) -> f64 {
+        let mut iter = 0;
+        let mut x0 = self.x0;
+
+        while iter < self.max_iter {
+           let x_next = (self.function)(x0);
+
+            if (x_next - x0).abs() < self.tolerance { return x_next; }
+
+            x0 = x_next;
+            iter +=1;
+        }
+
+        x0
+    }
+}
+
+impl InverseQuadraticInterpolation{
+    fn new(function: fn(f64) -> f64, x0: f64, x1: f64, x2: f64, tolerance: f64, max_iter: usize)
+        ->Self{ InverseQuadraticInterpolation{ function, x0, x1, x2, tolerance, max_iter } }
+}
+
+impl FindRoot for InverseQuadraticInterpolation {
+    fn solve(&mut self) -> f64 {
+        let mut iter = 0;
+        let mut x0 = self.x0;
+        let mut x1 = self.x1;
+        let mut x2 = self.x2;
+
+        while iter < self.max_iter {
+            let f0 = (self.function)(self.x0);
+            let f1 = (self.function)(self.x1);
+            let f2 = (self.function)(self.x2);
+
+            let a = f0 / ((f1 - f0) * (x2 - x0) - (f2 - f0) * (x1 - x0));
+            let b = f1 / ((f0 - f1) * (x2 - x1) - (f2 - f1) * (x0 - x1));
+            let c = f2 / ((f0 - f2) * (x1 - x2) - (f1 - f2) * (x0 - x2));
+
+            let x_next = x0 * a + x1 * b + x2 *c;
+            
+            if (x_next - x1).abs() < self.tolerance { return x_next; }
+
+            x0 = x1;
+            x1 = x2;
+            x2 = x_next;
+            iter +=1;
+        }
+
+        x2
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,75 @@
 mod find_root_bracketing;
 mod find_root_iterative;
 
+//use find_root_bracketing::{Bisection, FalsePosition, ITP};
+//use find_root_iterative::{Secant, Newton, Steffensen};
+use std::io;
+use std::process::Command;
+use std::io::Error;
+
+
 fn main() {
-    println!(" --- ROOT-FINDING ALGORITHMS ---");
-    println!("1. BRACKETING METHODS");
-    println!("1.1. Bisection");
-    println!("1.2. False position");
-    println!("1.3. ITP Method");
-    println!("2. ITERATIVE METHODS");
-    println!("2.1. Newton's method ");
-    println!("2.2. Secant method");
-    println!("2.3. Steffensen's method");
-    println!("2.4. Fixed point iteration method");
-    println!("2.5. Inverse interpolation");
-    println!("3. COMBINATION OF METHODS");
-    println!("3.1. Brent's method");
-    println!("3.2. Ridder's method");
+
+    if let Err(e) = clear_console() {
+        eprintln!("Error clearing console: {}", e);
+    }
+    let mut input = String::new();
+
+    menu();
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        eprintln!("Error al leer la línea: {}", e);
+        return;
+    }
+
+    let opt= input.trim();
+
+    loop {
+        match opt {
+            "1" => { println!("1"); break; },
+            "2" => { println!("2"); break; },
+            "3" => { println!("3"); break; },
+            "4" => {println!("4"); break;}
+            _ => {println!("_");
+            break;
+            todo!()}
+        }
+    }
+}
+
+
+fn menu (){
+    println!("::: :::--- ROOT-FINDING ALGORITHMS ---::: :::");
+    println!("::: :::--- EXAMPLES ---::: :::");
+    println!("::: ::: BRACKETING METHODS ::: :::");
+    println!("1. Bisection");
+    println!("2. False position");
+    println!("3. ITP Method");
+    println!("::: ::: ITERATIVE METHODS ::: :::");
+    println!("4. Newton's method ");
+    println!("5. Secant method");
+    println!("6. Steffensen's method");
+    println!("7. Fixed point iteration method");
+    println!("8. Inverse quadratic interpolation");
+    println!("::: ::: COMBINATION OF METHODS ::: :::");
+    println!("9. Brent's method");
+    println!("10. Ridder's method");
+    println!("--- --- --- --- ---");
+    println!("[0] EXIT");
+    println!("--- --- --- --- ---");
+    println!("--- --- --- --- ---");
+    println!("--- --- --- --- ---");
+    println!("Select an option: ");
+}
+
+fn clear_console() -> Result<(), Error> {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/c")
+            .arg("cls")
+            .status()?;
+    } else {
+        Command::new("clear")
+            .status()?;
+    }
+    Ok(())
 }
