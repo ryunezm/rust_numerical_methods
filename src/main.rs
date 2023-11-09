4c866ccffb8c6ce412c9746e97d87155a2852f06
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-11-09 17:49:17 -0500

    --> Cleaning and preparing code for numerical methods.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-mod find_roots;
-use std::io::{stdin, Error};
+use std::io::{Error, stdin};
 use std::process::Command;
+
+mod find_roots;
 
 fn main() {
 
@@ -46,23 +47,24 @@ fn main() {
 }
 
 fn menu_principal() {
-    println!("::: ::: ::: NUMERICAL METHODS: EXAMPLES ::: ::: :::");
+    println!("::: ::: ::: ::: NUMERICAL METHODS: EXAMPLES ::: ::: ::: :::");
     println!("1. Solving systems of linear equations");
     println!("2. Root finding algorithms");
+    println!("3. Quasi-Newton methods");
     println!("[0] EXIT");
     println!("::: ::: ::: ::: ::: :::");
     println!("Select an option: ");
 }
 
 fn menu_sle(){
-    println!("::: :::--- SOLVING SYSTEMS OF LINEAR EQUATIONS ---::: :::");
+    println!("::: :::--- SOLVING SYSTEMS OF LINEAR EQUATIONS ::: ::: :::");
     println!("1. Jacobi iteration method");
     println!("2. Gauss-Jordan method");
     println!("3. Gauss-Seidel method");
     println!("4. Successive over-relaxation method (SOR)")
 }
 fn menu_find_root(){
-    println!("::: :::--- ROOT-FINDING ALGORITHMS ---::: :::");
+    println!("::: ::: ::: ROOT-FINDING ALGORITHMS ::: ::: :::");
     println!("::: ::: BRACKETING METHODS ::: :::");
     println!("1. Bisection");
     println!("2. False position");
@@ -77,6 +79,15 @@ fn menu_find_root(){
     println!("9. Brent's method");
     println!("10. Ridder's method");
     println!("--- --- --- --- ---");
+}
+
+fn menu_quasi_newton_method(){
+    println!("::: ::: ::: QUASI-NEWTON METHODS ::: ::: :::");
+    println!("1. Broyden's method");
+    println!("2. Broyden–Fletcher–Goldfarb–Shanno (BFGS) algorithm");
+    println!("3. Limited-memory BFGS (LM-BFGS) algorithm");
+    println!("4. Davidon–Fletcher–Powell (DFP) formula");
+    println!("5. Symmetric rank-one")
 }
 
 fn clear_console() -> Result<(), Error> {
